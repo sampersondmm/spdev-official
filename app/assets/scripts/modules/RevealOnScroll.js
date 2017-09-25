3a478428a439e23eff56d4ce65f7486bbe713fe9
sam <sampersondmm@gmail.com>
2017-09-25 11:21:01 -0400

    newest version

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $(".reveal");
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal");
+  }
+
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal--is-visible")
+        },
+        offset: "85%"
+      });
+    });
+  }
+}
+export default RevealOnScroll;
